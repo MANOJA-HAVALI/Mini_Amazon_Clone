6b1cef109f82eeb4fe32c942fea80874d4a90245
MANOJ-HAVLI <153119909+MANOJ-HAVLI@users.noreply.github.com>
2026-01-27 16:36:41 +0530

    exception modified

---
+++ Mini-Amazon-Clone/src/main/java/com/example/Mini/Amazon/Clone/exception/GlobalExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.example.Mini.Amazon.Clone.exception;
+
+import com.example.Mini.Amazon.Clone.dto.requests.responses.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleGlobalExc(Exception e) {
+        log.error(e.getMessage());
+        ApiResponse<String> errordetails = new ApiResponse<>("false", e.getMessage());
+        return new ResponseEntity<>(errordetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidationException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = ex.getBindingResult().getFieldErrors()
+                .stream().collect(Collectors.toMap(
+                        err -> err.getField(),
+                        err -> err.getDefaultMessage(),
+                        (existing, replacement) -> existing
+                ));
+
+        ApiResponse<Map<String, String>> errordetails = new ApiResponse<>("false",errors);
+        return new ResponseEntity<>(errordetails, HttpStatus.BAD_REQUEST);
+    }
+}
